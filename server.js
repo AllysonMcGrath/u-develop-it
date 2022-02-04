0cc0fcd26e13ed7807730d5d2f8cf52fb8534592
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2022-02-03 23:13:40 -0600

    added voter routes

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-const db = require('./db/connection');
-const inputCheck = require('./utils/inputCheck');
 const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express();
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-//express middleware
-app.use(express.urlencoded({extended: false}));
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-
-//default response for any other request (not found)
+// Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
-})
+  res.status(404).end();
+});
 
-
+// Start server after DB connection
 db.connect(err => {
-    if (err) throw err;
-    console.log('Database connected.');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
-
+});
